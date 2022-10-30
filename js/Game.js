2708ff0b05852ffb152a754280a17117b539630f
MJ <code.za@proton.me>
2022-10-30 09:32:13 +0200

    Update

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,23 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+//create the Game class
+class Game {
+    //Game class parameters (outside of constructor) - see guide.
+    //see Step 3
+    missed = 0;
+    phrases = [ 
+        {phrase: 'Life is like a box of chocolates'},
+        {phrase: 'Home is where the heart is'},
+        {phrase: 'Fly away chariot'},
+        {phrase: 'Gone with the wind'},
+        {phrase: 'Seven years in Tibet'}
+    
+    ];
+    activePhrase = null;
+
+    constructor () {
+
+    }
+}
--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,35 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+//create the Phrase class
+class Phrase {
+
+phrase = this.phrase; //not sure if this is correct
+
+    constructor (phrase) {
+        //not sure if this is correct. May need to do it outside of constructor - works for now
+        this.phrase = phrase.toLowerCase();
+    }
+
+    //method within Phrase
+    addPhraseToDisplay() {
+{/* <div id="phrase" class="section">
+    <ul>
+        <li class="hide letter h">h</li>
+        <li class="hide letter o">o</li>
+        <li class="hide letter w">w</li>
+        <li class="space"> </li>
+        <li class="hide letter a">a</li>
+        <li class="hide letter r">r</li>
+        <li class="hide letter e">e</li>
+        <li class="space"> </li>
+        <li class="hide letter y">y</li>
+        <li class="hide letter o">o</li>
+        <li class="hide letter u">u</li>
+    </ul>
+</div> */}
+
+    }
+
+}
