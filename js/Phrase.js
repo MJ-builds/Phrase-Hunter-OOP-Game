1c1521d18d6a5872674b1de884a08b794f73b8d4
MJ <code.za@proton.me>
2022-11-07 21:29:36 +0200

    Update

--- js/Phrase.js
+++ js/Phrase.js
@@ -4,48 +4,42 @@
 
 //create the Phrase class
 class Phrase {
+  // phrase = this.phrase; //not sure if this is correct
 
-// phrase = this.phrase; //not sure if this is correct
+  constructor(phrase) {
+    this.phrase = phrase.toLowerCase(); //revisit later
+  }
 
-    constructor (phrase) {
-        
-        this.phrase = phrase.toLowerCase(); //revisit later
-    }
+  //method within Phrase
+  addPhraseToDisplay() {
+    //split the random phrase into a new array 'LetterArray' of letters (of the phrase itself)
+    const letterArray = this.phrase.split("");
 
-    //method within Phrase
-    addPhraseToDisplay() {
+    //create template literal with opening unordered list tag
+    let html = `<ul>`;
 
-        const letterArray = this.phrase.split(''); //not sure if correct. NB. Check that working
-        //console.log(phraseLetters); for testing
-        let html = `<ul>`;
-
-letterArray.forEach(element => {
-    if(element ===" ") {
+    //loop through letters of the letterArray variable just created
+    letterArray.forEach((element) => {
+      if (element === " ") {
         html += `<li class="space">${element}</li>`;
-    }
-    else html += `<li class="hide letter ${element}">${element}</li>`
-});
-    //console.log(html); for testing
-    html += `</ul>`;
-    return document.getElementById('phrase').innerHTML = html;
-
-    }
-checkLetter(letter) {
-
-    const letterArray = this.phrase.split(''); //not sure if correct. NB. Check that working
-    letterArray.forEach(element => {
-        if(element === letter) {
-            console.log('true'); //for testing
-            return true;
-        }
-        else 
-        console.log('false'); //for testing
-        return false; //not sure if correct or in right place
+      } else html += `<li class="hide letter ${element}">${element}</li>`;
     });
-}
+//wrap the template literal (once looped through) in closing unordered list tag
+    html += `</ul>`;
+    return (document.getElementById("phrase").innerHTML = html);
+  }
+  checkLetter(letter) {
+    const letterArray = this.phrase.split(""); //not sure if correct. NB. Check that working
+    letterArray.forEach((element) => {
+      if (element === letter) {
+        console.log("true"); //for testing
+        return true;
+      } else console.log("false"); //for testing
+      return false; //not sure if correct or in right place
+    });
+  }
 
-showMatchedLetter(letter) {
-    let unhide = document.getElementById('phrase'); //experimental
-}
-
+  showMatchedLetter(letter) {
+    let unhide = document.getElementById("phrase"); //experimental
+  }
 }
--- js/app.js
+++ js/app.js
@@ -4,10 +4,10 @@
 
 let game;
 //may need to change variable name later on (maybe just to start)
-const startReset = document.getElementById('btn__reset');
+const startReset = document.getElementById("btn__reset");
 
 //on click, creat a new instance of Game class, and call startGame() method.
-startReset.addEventListener('click', e => {
-    game = new Game();
-    game.startGame();
+startReset.addEventListener("click", (e) => {
+  game = new Game();
+  game.startGame();
 });
